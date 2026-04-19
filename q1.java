d7b6ec89fda766d9faf7825db81148ab26d3ffaa
NagarathnaAS30 <nagarathnaas908@gmail.com>
2026-04-19 16:48:39 +0530

    Add files via upload

---
+++ q1.java
@@ -0,0 +1,32 @@
+package lab_3b;
+import java.util.Scanner;
+public class q1 {
+	
+	
+		public static boolean isNullOrEmpty(String str) {
+	        if (str == null || str.trim().isEmpty()) {
+	            return true;
+	        }
+	        return false;
+	    }
+
+	    public static void main(String[] args) {
+
+	        Scanner sc = new Scanner(System.in);
+
+	        System.out.println("Enter a string:");
+	        String input = sc.nextLine();
+
+	        if (isNullOrEmpty(input)) {
+	            System.out.println("The string is null or contains only whitespace");
+	        } else {
+	            System.out.println("The string is not null or empty");
+	        }
+
+	        sc.close();
+	    }
+	}
+
+		
+
+
